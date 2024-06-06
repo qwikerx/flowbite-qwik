97c20e024ba553134409b1091e63c74527f30c25
Jean-Charles FAUCHET <fauchet.jeancharles@gmail.com>
2024-06-06 18:01:00 +0200

    feat: add rating docs

---
+++ apps/web/src/routes/examples/[theme-rtl]/rating/01-default/index@examples.tsx
@@ -0,0 +1,21 @@
+/**
+ * title: Default rating
+ * description: Use this simple example of a star rating component for showing review results.
+ */
+
+import { component$ } from '@builder.io/qwik'
+import { Rating } from 'flowbite-qwik'
+import { staticGenerateHandler } from '../../layout'
+import { StaticGenerateHandler } from '@builder.io/qwik-city'
+
+export default component$(() => {
+  return (
+    <div class="p-3">
+      <Rating rating={4} />
+    </div>
+  )
+})
+
+export const onStaticGenerate: StaticGenerateHandler = async () => {
+  return staticGenerateHandler()
+}
