aed9c737fad05ee330247699813cea534a80048e
xmimiex <benjamin.henique@gmail.com>
2024-08-14 15:27:11 +0200

    fix: page content component (#179)

--- apps/web/src/components/DocumentPageContent/DocumentPageContent.tsx
+++ apps/web/src/components/DocumentPageContent/DocumentPageContent.tsx
@@ -1,8 +1,8 @@
-import { component$, Slot } from '@builder.io/qwik'
+import { component$, PropsOf, Slot } from '@builder.io/qwik'
 
-export const DocumentPageContent = component$(() => {
+export const DocumentPageContent = component$<PropsOf<'div'>>((attrs) => {
   return (
-    <div class="flex">
+    <div class="flex" {...attrs}>
       <div class="mx-auto flex min-w-0 max-w-4xl flex-col px-4 pb-12 pt-6 lg:px-8 lg:pb-16 lg:pt-8 xl:pb-24">
         <Slot name="top" />
       </div>
