a60a7699f7701778cdeb5e73da67ab36d1ffbf94
xmimiex <benjamin.henique@gmail.com>
2025-01-12 17:02:17 +0100

    feat: use iconLeft feature on Link component (#225)

--- apps/web/src/routes/examples/[theme-rtl]/link/04-link-icon/index.tsx
+++ apps/web/src/routes/examples/[theme-rtl]/link/04-link-icon/index.tsx
@@ -8,16 +8,23 @@ import { component$ } from '@builder.io/qwik'
 import { StaticGenerateHandler } from '@builder.io/qwik-city'
 import { Link } from 'flowbite-qwik'
 import { staticGenerateHandler } from '~/routes/examples/layout'
-import { IconAngleRightOutline } from 'flowbite-qwik-icons'
+import { IconAngleLeftOutline, IconAngleRightOutline } from 'flowbite-qwik-icons'
 
 export default component$(() => {
   return (
-    <p class="text-gray-500 dark:text-gray-400">
-      500,000 people & companies have made over a million apps with Glide.{' '}
-      <Link href="#" iconRight={IconAngleRightOutline}>
-        Read their stories
-      </Link>
-    </p>
+    <>
+      <p class="text-gray-500 dark:text-gray-400">
+        500,000 people & companies have made over a million apps with Glide.{' '}
+        <Link href="#" iconRight={IconAngleRightOutline}>
+          Read their stories
+        </Link>
+      </p>
+      <p>
+        <Link href="#" iconLeft={IconAngleLeftOutline}>
+          Back to the future
+        </Link>
+      </p>
+    </>
   )
 })
 
